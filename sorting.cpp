43c7b2326b8a4ab0b7e132c0e701f7e3bbcb1d0b
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2026-02-04 11:21:48 +0100

    added merge_sort to sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -42,22 +42,67 @@ void selection_sort(std::vector<int> &vec) {
   }
 }
 
-
 void insertion_sort(std::vector<int> &vec) {
   int n = vec.size();
   for (int i = 1; i < n; ++i) {
-      int key = vec[i];
-      int j = i - 1;
-      while (j >= 0 && vec[j] > key) {
-          vec[j + 1] = vec[j];
-          --j;
-      }
-      vec[j + 1] = key;
+    int key = vec[i];
+    int j = i - 1;
+    while (j >= 0 && vec[j] > key) {
+      vec[j + 1] = vec[j];
+      --j;
+    }
+    vec[j + 1] = key;
   }
 }
 
-void merge_sort(std::vector<int> &vec) {
+void merge(std::vector<int> &vec, int left, int mid, int right) {
+  int n1 = mid - left + 1;
+  int n2 = right - mid;
 
+  std::vector<int> l(n1), r(n2);
+
+  for (int i = 0; i < n1; ++i)
+    l[i] = vec[left + i];
+  for (int j = 0; j < n2; ++j)
+    r[j] = vec[mid + j + 1];
+
+  int i = 0, j = 0, k = left;
+
+  while (i < n1 && j < n2) {
+    if (l[i] <= r[j]) {
+      vec[k] = l[i];
+      i++;
+    } else {
+      vec[k] = r[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    vec[k] = l[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    vec[k] = r[j];
+    j++;
+    k++;
+  }
+}
+
+void run_merge_sort(std::vector<int> &vec, int left, int right) {
+  if (left >= right) return;
+  
+  int mid = left + (right - left) / 2;
+  run_merge_sort(vec, left, mid);
+  run_merge_sort(vec, mid + 1, right);
+  merge(vec, left, mid, right);
+}
+
+void merge_sort(std::vector<int> &vec) {
+  run_merge_sort(vec, 0, vec.size() - 1);
 }
 
 int main() {
