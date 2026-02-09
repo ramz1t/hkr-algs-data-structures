fe1333112afb55e6314d3d01ce1a07c5793f3726
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2026-02-09 13:31:38 +0100

    fixed s1/task1

--- seminar1/task1.cpp
+++ seminar1/task1.cpp
@@ -86,49 +86,46 @@ int median_of_three(std::vector<int>& a, int low, int high) {
   return mid;
 }
 
-// Pivot = first element
-void quick_sort_start_rec(std::vector<int>& a, int low, int high) {
-  if (low < high) {
-    int p = partition(a, low, high, low);
-    quick_sort_start_rec(a, low, p - 1);
-    quick_sort_start_rec(a, p + 1, high);
-  }
+using PivotFunc = std::function<int(std::vector<int>&, int, int)>;
+
+// Pivot functions
+int pivot_start(std::vector<int>&, int low, int) {
+  return low;
 }
 
-void quick_sort_start(std::vector<int>& vec) {
-  quick_sort_start_rec(vec, 0, vec.size() - 1);
+int pivot_rand(std::vector<int>&, int low, int high) {
+  return low + rand() % (high - low + 1);
 }
 
-// Pivot = random element
-void quick_sort_rand_rec(std::vector<int>& a, int low, int high) {
+int pivot_m3(std::vector<int>& a, int low, int high) {
+  return median_of_three(a, low, high);
+}
+
+// Generic recursive quick sort
+void quick_sort_rec(std::vector<int>& a, int low, int high, PivotFunc pickPivot) {
   if (low < high) {
-    int pivotIndex = low + rand() % (high - low + 1);
+    int pivotIndex = pickPivot(a, low, high);
     int p = partition(a, low, high, pivotIndex);
-    quick_sort_rand_rec(a, low, p - 1);
-    quick_sort_rand_rec(a, p + 1, high);
+
+    quick_sort_rec(a, low, p - 1, pickPivot);
+    quick_sort_rec(a, p + 1, high, pickPivot);
   }
+}
+
+// Recursive wrappers
+void quick_sort_start(std::vector<int>& vec) {
+  quick_sort_rec(vec, 0, vec.size() - 1, pivot_start);
 }
 
 void quick_sort_rand(std::vector<int>& vec) {
-  quick_sort_rand_rec(vec, 0, vec.size() - 1);
-}
-
-// Pivot = median of three
-void quick_sort_m3_rec(std::vector<int>& a, int low, int high) {
-  if (low < high) {
-    int pivotIndex = median_of_three(a, low, high);
-    int p = partition(a, low, high, pivotIndex);
-    quick_sort_m3_rec(a, low, p - 1);
-    quick_sort_m3_rec(a, p + 1, high);
-  }
+  quick_sort_rec(vec, 0, vec.size() - 1, pivot_rand);
 }
 
 void quick_sort_m3(std::vector<int>& vec) {
-  quick_sort_m3_rec(vec, 0, vec.size() - 1);
+  quick_sort_rec(vec, 0, vec.size() - 1, pivot_m3);
 }
 
-using PivotFunc = std::function<int(std::vector<int>&, int, int)>;
-
+// Generic iterative quick sort
 void quick_sort_iter(std::vector<int>& a, PivotFunc pickPivot) {
   std::vector<std::pair<int,int>> stack;
   stack.push_back({0, (int)a.size() - 1});
@@ -147,18 +144,7 @@ void quick_sort_iter(std::vector<int>& a, PivotFunc pickPivot) {
   }
 }
 
-int pivot_start(std::vector<int>&, int low, int) {
-  return low;
-}
-
-int pivot_rand(std::vector<int>&, int low, int high) {
-  return low + rand() % (high - low + 1);
-}
-
-int pivot_m3(std::vector<int>& a, int low, int high) {
-  return median_of_three(a, low, high);
-}
-
+// Iterative wrappers
 void quick_sort_start_iter(std::vector<int>& vec) {
   quick_sort_iter(vec, pivot_start);
 }
@@ -191,7 +177,7 @@ void run_test(int n, int repeats, const std::string& name, SortFunc sort_method)
 
 int main() {
   int repeats = 5;
-  int sizes[5] = {100, 1000, 10000, 100000, 1000000};
+  int sizes[] = {100, 1000, 10000, 100000, 1000000};
 
   for (int size : sizes) {
     run_test(size, repeats, "Quick start rec", quick_sort_start);
